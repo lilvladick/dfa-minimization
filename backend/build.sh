ab10d44f43e3b4f0c9fb02089275ffb648e44711
Владислав <103439608+lilvladick@users.noreply.github.com>
2025-04-21 11:41:15 +0300

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+set -e
+
 SOURCE_FILES="*.cpp"
 OUTPUT_FILE="dfa_minimizer"
-INCLUDE_DIRS="-Iinclude -I/opt/homebrew/include -I/usr/local/include -Iinclude -I/opt/homebrew/include/nlohmann"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    INCLUDE_DIRS="-Iinclude -I/opt/homebrew/include -I/usr/local/include -I/opt/homebrew/include/nlohmann"
+else
+    # Linux (Ubuntu)
+    INCLUDE_DIRS="-Iinclude -I/usr/local/include"
+fi
+
 CXX_FLAGS="-std=c++17 -pthread"
+
+echo "Текущая директория: $(pwd)"
+echo "Файлы:"
+ls -l
 
 echo "Compiling..."
 
 clang++ $CXX_FLAGS $INCLUDE_DIRS -o $OUTPUT_FILE $SOURCE_FILES
 
-if [ $? -eq 0 ]; then
-    echo "Compilation successful. Run with ./dfa_minimizer"
-else
-    echo "Compilation failed."
-    exit 1
-fi
+echo "Compilation successful. Run with ./$OUTPUT_FILE"
+exit 0
